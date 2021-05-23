46eb46305699ef53d4f3f9eb15eba0912597ab38
Rifqi Arief <33513626+rapdodge@users.noreply.github.com>
2021-05-23 15:49:26 +0700

    Memperbarui 'pengiriman.php'
    Menambahkan kurir AnterAja

--- pengiriman.php
+++ pengiriman.php
@@ -94,4 +94,102 @@ if ($Kurir == 'jne') {
 		$Hasil = array_merge($CekResi, $Keterangan, $Pengirim, $Penerima, $HasilRiwayat);
 		print_r(json_encode($Hasil));
 	}
+} elseif ($Kurir == 'anteraja') {
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "https://api.anteraja.id/order/tracking",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "POST",
+	  CURLOPT_POSTFIELDS =>"[{\"codes\":\"$Resi\"}]",
+	  CURLOPT_HTTPHEADER => array(
+	    "mv: 1.2",
+	    "source: aca_android",
+	    "content-type: application/json; charset=UTF-8",
+	    "user-agent: okhttp/3.10.0",
+	  ),
+	));
+
+	$ResponcURL = json_decode(curl_exec($curl), true);
+
+	curl_close($curl);
+	
+	if ($ResponcURL['content'][0]['detail']['final_status'] == 250) {
+		$StatusKirim = ' | DELIVERED';
+	} else {
+		$StatusKirim = ' | ON PROCESS';
+	}
+	
+
+	$CekResi = array();
+
+	if ($ResponcURL['status'] != 200) {
+		$CekResi['name'] = 'AnterAja';
+		$CekResi['site'] = 'anteraja.id';
+		$CekResi['error'] = true;
+		$CekResi['message'] = 'Nomor resi tidak ditemukan.';
+		print_r(json_encode($CekResi));
+	} else {
+		$CekResi['name'] = 'AnterAja';
+		$CekResi['site'] = 'anteraja.id';
+		$CekResi['error'] = false;
+		$CekResi['message'] = 'success';
+
+		$Keterangan = array(
+			'info' => array(
+				'no_awb' => $ResponcURL['content'][0]['awb'],
+				'service' => $ResponcURL['content'][0]['detail']['service_code'],
+				'status' => $StatusKirim,
+				'tanggal_kirim' => date('d-m-Y H:i', strtotime($ResponcURL['content'][0]['detail']['shipped_date'])),
+				'tanggal_terima' => date('d-m-Y H:i', strtotime($ResponcURL['content'][0]['detail']['delivered_date'])),
+				'harga' => $ResponcURL['content'][0]['detail']['actual_amount'],
+				'berat' => $ResponcURL['content'][0]['detail']['weight'],
+				'catatan' => $ResponcURL['content'][0]['items'][0]['name'],
+			),
+		);
+
+		$Pengirim = array(
+			'pengirim' => array(
+				'nama' => $ResponcURL['content'][0]['detail']['sender']['name'],
+				'phone' => null,
+				'alamat' => null,
+			),
+		);
+
+		$Penerima = array(
+			'penerima' => array(
+				'nama' => $ResponcURL['content'][0]['detail']['receiver']['name'],
+				'nama_penerima' => $ResponcURL['content'][0]['detail']['actual_receiver'],
+				'phone' => null,
+				'alamat' => null,
+			),
+		);
+
+		$BalikRiwayat = array_reverse($ResponcURL['content'][0]['history']);
+		$HitungRiwayat = count($BalikRiwayat);
+		$Riwayat = array();
+		foreach ($BalikRiwayat as $k => $v) {
+			$Riwayat[$k]['tanggal'] = date('d-m-Y H:i', strtotime($BalikRiwayat[$k]['timestamp']));
+			if (preg_match('/Delivery sukses/', $BalikRiwayat[$k]['message']['id'])) {
+				$Riwayat[$k]['posisi'] = null;
+				$Riwayat[$k]['message'] = 'DELIVERED';
+			} else {
+				$Riwayat[$k]['posisi'] = null;
+				$Riwayat[$k]['message'] = $BalikRiwayat[$k]['message']['id'];
+			}
+			
+		}
+
+		$HasilRiwayat = array(
+			'history' => $Riwayat,
+		);
+
+		$Hasil = array_merge($CekResi, $Keterangan, $Pengirim, $Penerima, $HasilRiwayat);
+		print_r(json_encode($Hasil));
+	}
 }
